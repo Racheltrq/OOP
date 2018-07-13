486841d07e7383d17d8a1940025ff8a4affc34d2
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-13 17:30:11 -0400

    switch files

--- inclass/token.cpp
+++ inclass/token.cpp
@@ -1,7 +1,4 @@
 #include "std_lib_facilities.h"
-double expression();
-double primary();
-double term();
 
 class Token{
 	public:
@@ -9,94 +6,43 @@ class Token{
 		double value;
 };
 
-class Token_stream{
-	public:
-		Token get();
-		void putback(Token t);
-	private:
-		bool full{false};
-		Token buffer;
-};
+Token get_token();
 
-void Token_stream::putback(Token t){
-	buffer = t;
-	full = true;
+vector<Token> tokens;
+
+int main(){
+	Token t = get_token();
+	while(t.kind != 'q'){
+		tokens.push_back(t);
+		t = get_token();
+	}
+	for(Token tok: tokens){
+		cout << "Token kind = " << tok.kind << "; val = " << tok.value << '\n';
+	}
 }
 
-Token Token_stream::get(){
-	if (full){
-		full = false;
-		return buffer;
-	}
-		char ch;
-		cin >> ch;
-		switch(ch){
-			case ';':
-			case 'q':
-			case '(':
-			case ')':
-			case '+':
-			case '-':
-			case '*':
-			case '/':
-			return Token{ch};
-			case '.':
-			case '0': case '1': case '2': case '3': case '4':
-			case '5': case '6': case '7': case '8': case '9':
-			{
-				cin.putback(ch);
-				double val;
-				cin >> val;
-				return Token{'8', val};
-			}
-
+Token get_token(){
+	char ch;
+	cin >> ch;
+	switch(ch){
+		case ';':
+		case 'q':
+		case '(':
+		case ')':
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		return Token{ch};
+		case '.':
+		case '0': case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9':
+		{
+			cin.putback(ch);
+			double val;
+			cin >> val;
+			return Token{'8', val};
 		}
-}
 
-
-Token_stream ts;
-
-double expression(){
-	double left = term();
-	Token t = ts.get();
-	while(true){
-		switch(t.kind){
-			case '+':
-				return left + term();
-				break;
-			case '-':
-				return left - term();				
-				break;
-			default:
-				ts.putback(t);
-				return left;
-		}
 	}
 }
-
-double term(){
-	return primary();
-}
-
-double primary(){
-	Token t = ts.get();
-	return t.value;
-}
-
-
-int main(){	
-	double val = 0.0;
-	while (cin){
-		Token t = ts.get();
-		cout << "Got token: " << t.kind << " with val of " << t.value << '\n';
-		if(t.kind == 'q') break;
-		if(t.kind == ';') cout << " = " << val << '\n';
-			else ts.putback(t);
-		val = expression();
-	}
-	cout << "\nCALCULATOR TERMINATED\n";
-	return 0
-}
-
-
-
