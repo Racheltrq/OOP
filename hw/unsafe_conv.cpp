8c73420eb5921ad0651f1ec0a84007cef6c7bbea
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-18 10:26:21 -0400

    hw1 revision

--- hw/unsafe_conv.cpp
+++ hw/unsafe_conv.cpp
@@ -1,6 +1,47 @@
 #include "std_lib_facilities.h"
 
 int main(){
-	double a = 2.2;
-	cout << int a;
+	double a = 111111111111111112;
+	int aNarrowed = a;
+    double aBroader = aNarrowed;
+    cout << "double: " << a << "\n"
+        << "narrowed int: " << aNarrowed << "\n"
+        << "broader double: " << aBroader << "\n";
+
+    double b = 2.556;
+    char bNarrowed = b;
+    double bBroader = bNarrowed;
+    cout << "double: " << b << "\n"
+        << "narrowed char: " << bNarrowed << "\n"
+        << "broader double: " << bBroader << "\n";
+
+    double c = 2.556;
+    bool cNarrowed = c;
+    double cBroader = cNarrowed;
+    cout << "double: " << c << "\n"
+        << "narrowed boolean: " << cNarrowed << "\n"
+        << "broader double: " << cBroader << "\n";
+
+    int d = 3345;
+    char dNarrowed = d;
+    int dBroader = dNarrowed;
+    cout << "int: " << d << "\n"
+        << "narrowed char: " << dNarrowed << "\n"
+        << "broader int: " << dBroader << "\n";
+
+    int e = 3;
+    bool eNarrowed = e;
+    int eBroader = eNarrowed;
+    cout << "int: " << e << "\n"
+        << "narrowed boolean: " << eNarrowed << "\n"
+        << "broader int: " << eBroader << "\n";
+
+
+    char f = 'a';
+    bool fNarrowed = f;
+    char fBroader = fNarrowed;
+    cout << "char: " << f << "\n"
+        << "narrowed boolean: " << fNarrowed << "\n"
+        << "broader char: " << fBroader << "\n";
+
 }
