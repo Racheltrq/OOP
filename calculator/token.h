fa8ff9ef06cf9cde27389be9a98f497fb43c4bda
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-23 10:48:46 -0400

    test

--- calculator/token.h
+++ calculator/token.h
@@ -1,3 +1,11 @@
+const char invalid = 'i';
+const char name = 'a';
+const char number = '8';
+const char quit = 'q';
+const char print = ';';
+const char power = '^';
+const char mod = '%';
+
 class Token{
 	public:
 		char kind;
