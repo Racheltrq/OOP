b8d948cb807fe0fa067eaa534148a3e7963b29fd
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-26 10:57:32 -0400

    test add_day

--- date/date.cpp
+++ date/date.cpp
@@ -19,11 +19,12 @@ void Date::add_month(int n){
 }
 
 void Date::add_day(int n){
-	//d += n;
-	//if(d > month[int(m) - 1]){
-	//	m = Month(int(m) + 1);
-
-	//}
+	d += n;
+	numDay = month[int(m) - 1]
+	while(d > numDay){
+		d -= month[int(m)];
+		m = Month(int(m) + 1);
+	}
 }
 
 int Date::year(){
