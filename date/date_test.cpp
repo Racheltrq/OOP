b24103cd7844ab244be356c631980da36ac57a2a
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-25 23:42:08 -0400

    fix error

--- date/date_test.cpp
+++ date/date_test.cpp
@@ -7,7 +7,7 @@ vector<int> leap_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 int main(){
 	Month a = Month::jul;
 	Date today{2018, a, 20};
-	cout << "Today: " << today.year() << "/" << a << "/" 
+	cout << "Today: " << today.year() << "/" << int(today.month()) << "/" 
 		<< today.day() << endl;
 	return 0;
 }
