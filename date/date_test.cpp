ea06c6c1da63a58d8686ec7e1175130d01d6784d
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-25 23:07:12 -0400

    fix error

--- date/date_test.cpp
+++ date/date_test.cpp
@@ -2,7 +2,7 @@
 #include "date.h"
 
 vector<int> month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-vector<int> month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+vector<int> leap_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int main(){
 	Month a = Month::jul;
