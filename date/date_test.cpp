6aaf78a76d39dc171c3c44f6082c702ef6103730
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-26 12:18:21 -0400

    test add_day
    compile successed

--- date/date_test.cpp
+++ date/date_test.cpp
@@ -5,5 +5,7 @@ int main(){
 	Date today{2018, Month::jul, 20};
 	cout << "Today: " << today.year() << "/" << int(today.month()) << "/" 
 		<< today.day() << endl;
+	today.add_day(1);
+	cout << today.day() << endl;
 	return 0;
 }
