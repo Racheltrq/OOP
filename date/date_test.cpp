29b30651fde6fab3603934a28dc1ac8496e019c4
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-26 11:10:34 -0400

    keep fixing errors

--- date/date_test.cpp
+++ date/date_test.cpp
@@ -1,12 +1,10 @@
 #include "std_lib_facilities.h"
 #include "date.h"
 
-vector<int> month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-vector<int> leap_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
 
 int main(){
-	Month a = Month::jul;
-	Date today{2018, a, 20};
+	Date today{2018, Month::jul, 20};
 	cout << "Today: " << today.year() << "/" << int(today.month()) << "/" 
 		<< today.day() << endl;
 	return 0;
