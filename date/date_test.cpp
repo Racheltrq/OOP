e3c2024b360f2fe4a85836d7028aba6206b13271
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-27 15:47:19 -0400

    finished

--- date/date_test.cpp
+++ date/date_test.cpp
@@ -5,20 +5,21 @@ int main(){
 	cout << "Please enter a date in format (YYYY,MM,DD): ";
 	Date new_day;
 	cin >> new_day;
+
 	cout << "How many days do you want to add? ";
 	int addDay;
 	cin >> addDay;
 	new_day.add_day(addDay);
 	cout << new_day << endl;
+
 	cout << new_day << " == (Please enter another date): ";
 	Date day2;
 	cin >> day2;
-	cout << "\n";
 	if (new_day == day2) cout << "Same date." << endl;
 	else cout << "Different date" << endl;
+	
 	cout << new_day << " != (Please enter another date): ";
 	cin >> day2;
-	cout << "\n";
 	if (new_day != day2) cout << "Different date." << endl;
 	else cout << "Same date" << endl;
 	return 0;
