a415c009971be0bfdb74a2764ea5dd2b1aa71c98
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-27 13:53:10 -0400

    fix invalid day

--- date/date.cpp
+++ date/date.cpp
@@ -57,8 +57,8 @@ std::ostream& operator<<(ostream& os, Date& dt){
 
 bool Date::isvalid(){
 	if((int(m) < 1) || (int(m) > 12) || (d < 1)) return false;
-	else if(isleap(y) && (d > leap_month_days[m-1])) return false;
-	else if((!isleap()) && (d > month_days[m-1])) return false;
+	else if(isleap(y) && (d > leap_month_days[int(m)-1])) return false;
+	else if((!isleap()) && (d > month_days[int(m)-1])) return false;
 	else return true;
 }
 
