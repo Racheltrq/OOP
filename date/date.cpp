ccc98bbfc70233ba66a8e89cacf9665deb298981
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-27 11:18:20 -0400

    fix error

--- date/date.cpp
+++ date/date.cpp
@@ -53,7 +53,7 @@ Month Date::operator++(int){
 	return m;
 }
 
-ostream& operator<<(istream& os, const Date& dt){
+ostream& Date::operator<<(istream& os, const Date& dt){
 	os << count_mon[int(dt.month())] << " " << dt.day() << " " << dt.year();
 }
 
