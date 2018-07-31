af1b0bbd26139e8d7b6e22d5f3cef4ece61adda9
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-31 10:54:45 -0400

    testing

--- inclass/pointer.cpp
+++ inclass/pointer.cpp
@@ -8,9 +8,9 @@ int main(){
 	double* dp = &d;
 	int* ip = &i;
 	cout << cp << " has a value of " << *cp << endl;
-	cout << "size of cp is: " << sizeof(cp) << endl;
+	cout << "size of cp is: " << sizeof(cp) << " and size of c is: " << sizeof(c) << endl;
 	cout << dp << " has a value of " << *dp << endl;
-	cout << "size of dp is: " << sizeof(dp) << endl;
+	cout << "size of dp is: " << sizeof(dp) << " and size of d is: " << sizeof(d) << endl;
 	cout << ip << " has a value of " << *ip << endl;
-	cout << "size of ip is: " << sizeof(ip) << endl;
+	cout << "size of ip is: " << sizeof(ip) << " and size of c is: " << sizeof(i) << endl;
 }
