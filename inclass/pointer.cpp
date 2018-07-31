5decb8d774fecd25a0546aced8d0711c1cf50de1
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-07-31 10:50:27 -0400

    fixing error

--- inclass/pointer.cpp
+++ inclass/pointer.cpp
@@ -5,12 +5,12 @@ int main(){
 	int i = 1212;
 	double d = 3.14;
 	char* cp = &c;
-	//double* dp = &dp;
+	double* dp = &d;
 	int* ip = &i;
 	cout << cp << " has a value of " << *cp << endl;
 	cout << "size of cp is: " << sizeof(cp) << endl;
-	//cout << dp << " has a value of " << *dp << endl;
-	//cout << "size of dp is: " << sizeof(dp);
+	cout << dp << " has a value of " << *dp << endl;
+	cout << "size of dp is: " << sizeof(dp) << endl;
 	cout << ip << " has a value of " << *ip << endl;
 	cout << "size of ip is: " << sizeof(ip) << endl;
 }
