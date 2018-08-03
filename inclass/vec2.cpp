897d27e23d620e83fe541a3daa0cab5e17d6fe34
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-08-03 12:47:48 -0400

    testing

---
+++ inclass/vec2.cpp
@@ -0,0 +1,30 @@
+#include "std_lib_facilities.h"
+
+class vect{
+	int sz;
+	double* elem;
+public:
+	double get(int i){
+		return elem[i];
+	}
+	void set(int i, double d){
+		elem[i] = d;
+	}
+	vect(int s):sz{s}, elem{new double [s]}{}
+	vect(const vect&);
+
+	~vect(){delete elem;}
+};
+
+
+vect::vect(const vect& arg):sz{arg.sz}, elem{new double[arg.sz]}{
+	copy(arg.elem, arg.elem + arg.sz, elem);
+}
+
+vect& vect::operator=(cont vect& a){
+	double* p = new double[a.sz];
+	copy(a.elem, a.elem, p);
+	delete[] elem;
+	sz = a.sz;
+	return *this;
+}
