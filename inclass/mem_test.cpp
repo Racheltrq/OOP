4eff9e5f7a9896d280a8b2b9ce9e7e61bcff9aee
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-08-05 23:12:03 -0400

    fix error

--- inclass/mem_test.cpp
+++ inclass/mem_test.cpp
@@ -1,9 +1,10 @@
 #include "std_lib_facilities.h"
 
 int main(){
-	double* arr = new double[10000];
+	int* arr = new double[10000];
+	
 	for(int i = 0; i < 10000; i++){
-		arr[i] = new double;
+		arr[i] = i;
 		cout << i << endl;
 	}
 }
