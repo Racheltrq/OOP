59b93c5de8c26850da702f164c9ad1581c527f78
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-08-05 23:24:33 -0400

    test

--- inclass/mem_test.cpp
+++ inclass/mem_test.cpp
@@ -1,13 +1,10 @@
 #include "std_lib_facilities.h"
 
 int main(){
-	count = 0;
+	int count = 0;
 	while(true){
-	double* arr = new double[10000];
-	for(int i = 0; i < 10000; i++){
-		arr[i] = i;
+		double* arr = new double[10000];
 		count++;
 		cout << count << endl;
 	}
-}
 }
