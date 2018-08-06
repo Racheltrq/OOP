13e18b91e6798230b85aea4b9e7f7e612c786f8d
Racheltrq <35946574+Racheltrq@users.noreply.github.com>
2018-08-05 23:19:13 -0400

    fixing

--- inclass/mem_test.cpp
+++ inclass/mem_test.cpp
@@ -1,10 +1,13 @@
 #include "std_lib_facilities.h"
 
 int main(){
+	count = 0;
+	while(true){
 	double* arr = new double[10000];
-	
 	for(int i = 0; i < 10000; i++){
 		arr[i] = i;
-		cout << i << endl;
+		count++;
+		cout << count << endl;
 	}
+}
 }
